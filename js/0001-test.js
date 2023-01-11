5b0124950ffd7cca980c69de49b54c3ce75e1355
DenisPepper <thech@yandex.ru>
2023-01-11 21:33:37 +0300

    minor fixes

--- js/0001-test.js
+++ js/0001-test.js
@@ -1,7 +1,7 @@
 export const test = () => {
     
     const a = document.createElement('a');
-    a.href = '#';
+    a.href = 'https://www.codewars.com/kata/55a2d7ebe362935a210000b2/train/javascript';
     a.textContent = 'my last not resolved chellenge';
     a.style.display = 'block';
     a.style.paddingTop = '30px';
