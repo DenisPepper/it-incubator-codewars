3f0afe7b00b3d6434d1b5af9f62a481f2309a73a
DenisPepper <thech@yandex.ru>
2023-01-12 21:22:32 +0300

    function greet

---
+++ js/function-hello-world.js
@@ -0,0 +1,6 @@
+/*
+https://www.codewars.com/kata/523b4ff7adca849afe000035/train/javascript
+
+Make a simple function called greet that returns the most-famous "hello world!".
+*/
+const greet = () => 'hello world!';
