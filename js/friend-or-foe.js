9cf60357b1304590248bde2149c35ee01a08e5c9
DenisPepper <thech@yandex.ru>
2023-01-12 21:16:27 +0300

    Friend or Foe?

---
+++ js/friend-or-foe.js
@@ -0,0 +1,9 @@
+/*
+https://www.codewars.com/kata/55b42574ff091733d900002f/train/javascript
+
+friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+*/
+
+function friend(friends){
+    return friends.filter(name => name.length === 4);
+  }
