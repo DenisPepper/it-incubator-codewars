ad9eb16b14127e9a4d6af37b7611eadd071f75a1
DenisPepper <thech@yandex.ru>
2023-01-14 10:24:38 +0300

    Descending Order

---
+++ js/descending-order.js
@@ -0,0 +1,11 @@
+/*
+https://www.codewars.com/kata/5467e4d82edf8bbf40000155/train/javascript
+*/
+
+function descendingOrder(n){
+    return Number.parseInt(
+         [...n.toString()]
+         .sort((a, b) => b - a)
+         .join('')
+       );
+ }
